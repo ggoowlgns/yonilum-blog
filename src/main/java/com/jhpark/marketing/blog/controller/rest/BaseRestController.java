657cb75fbb7a76923d4f3634bcd7b6f96d033341
Park-Ji-Hoon <ggoowlgns@naver.com>
2021-09-05 00:10:04 +0900

    add alpha properties executable jar 생성 1. 일단 ci/cd flow 는  local 에서 만들고 ftp 로 instance 로 올리는 방식으로 하자.

--- src/main/java/com/jhpark/marketing/blog/controller/rest/BaseRestController.java
+++ src/main/java/com/jhpark/marketing/blog/controller/rest/BaseRestController.java
@@ -1,15 +1,25 @@
 package com.jhpark.marketing.blog.controller.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class BaseRestController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-  @RequestMapping(path = "/api", method = RequestMethod.GET)
-  public HttpStatus test() {
-    return HttpStatus.OK;
+@RestController
+@RequestMapping(path = "/api")
+public class BaseRestController {
+  Logger LOG = LoggerFactory.getLogger(BaseRestController.class);
+
+  @RequestMapping(path = "/test", method = RequestMethod.GET, produces = "application/json")
+  public String test(HttpServletRequest request, HttpServletResponse response,
+                     @RequestParam("a") String a, @RequestParam("b") String b) {
+    LOG.info("response body = request query string");
+    return request.getQueryString();
   }
 }
