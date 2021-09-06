7c21d328cc38ea0e115cb4a2514e95310aa72c7e
Park-Ji-Hoon <ggoowlgns@naver.com>
2021-09-07 01:19:19 +0900

    WIP : OAuth 인증 도입 시작  - ref : https://ozofweird.tistory.com/entry/Spring-Boot-Spring-Boot-JWT-OAuth2-2

--- README.md
+++ README.md
@@ -6,7 +6,10 @@ yonilum project 의 blog app 소스입니다.
 
 ### Used Frameworks, Library
 * Spring Boot MVC
-* jpa
+* jpa / jdbc
+* security, oauth2.0
 * freemarker
 
 
+### REF
+* OAuth2.0 : https://ozofweird.tistory.com/entry/Spring-Boot-Spring-Boot-JWT-OAuth2-2
--- src/main/java/com/jhpark/marketing/blog/domain/Posting.java
+++ src/main/java/com/jhpark/marketing/blog/domain/Posting.java
@@ -1,11 +1,10 @@
 package com.jhpark.marketing.blog.domain;
 
-import com.sun.istack.internal.NotNull;
 import lombok.Data;
 
 @Data
 public class Posting {
-  private @NotNull long postingSeq;
+  private long postingSeq;
   private String title;
   private String content;
 
