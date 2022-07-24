daaf47fdad44c34686b4a91a79b02c03b68396fe
jhpark-mac <ggoowlgns@gmail.com>
2022-07-24 17:25:48 +0900

    feat(token filter 타는 로직 제거): using shoudNotFilter
    - /assets 으로 시작하는 path 는 tokenFilter 를 타지 않는다.

--- src/main/java/com/jhpark/marketing/blog/filter/CookieAttributeFilter.java
+++ src/main/java/com/jhpark/marketing/blog/filter/CookieAttributeFilter.java
@@ -1,5 +1,6 @@
 package com.jhpark.marketing.blog.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -9,8 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
 
+@Slf4j
 class CookieAttributeFilter implements Filter {
-  private final Logger log = LoggerFactory.getLogger(CookieAttributeFilter.class);
   @Override public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
     HttpServletResponse httpServletResponse = (HttpServletResponse) response;
--- src/main/java/com/jhpark/marketing/blog/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/jhpark/marketing/blog/filter/TokenAuthenticationFilter.java
@@ -4,15 +4,19 @@ import com.jhpark.marketing.blog.configuration.OAuth2LoginSecurityConfig;
 import com.jhpark.marketing.blog.service.oauth2.TokenProvider;
 import com.jhpark.marketing.blog.service.oauth2.CustomUserDetailsService;
 import com.jhpark.marketing.blog.util.CookieUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -20,19 +24,18 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
-  @Autowired
-  private TokenProvider tokenProvider;
-
-  @Autowired
-  private CustomUserDetailsService customUserDetailsService;
-
-  private static final Logger logger = LoggerFactory.getLogger(TokenAuthenticationFilter.class);
+  private final TokenProvider tokenProvider;
+  private final CustomUserDetailsService customUserDetailsService;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    ContentCachingRequestWrapper contentRequestWrapper = new ContentCachingRequestWrapper(request);
+
     try {
       String jwt = getJwtFromRequest(request);
       if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
@@ -45,10 +48,11 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
         request.setAttribute("user", customUserDetailsService.getUserById(userId));
       }
     } catch (Exception ex) {
-      logger.error("Could not set user authentication in security context", ex);
+      log.error("Could not set user authentication in security context", ex);
     }
 
     filterChain.doFilter(request, response);
+    log.info("http uri : {}", contentRequestWrapper.getRequestURI());
   }
 
   /**
@@ -77,5 +81,14 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
       return token;
     }
     return null;
+  }
+
+  @Override
+  protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    String path = request.getRequestURI();
+    return isPathFiltalbe(path);
+  }
+  private boolean isPathFiltalbe(String path) {
+    return path.indexOf("/assets") >= 0;
   }
 }
